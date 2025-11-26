9c995f452343c692dd54df42e8b1112e6009b868
m-delgado56 <soybienpro64@hotmail.com>
2025-11-26 13:13:31 -0500

    first version(no changes)

---
+++ java-meet/src/main/java/com/epam/rd/autotasks/meetautocode/HelloJava.java
@@ -0,0 +1,8 @@
+package com.epam.rd.autotasks.meetautocode;
+
+public class HelloJava {
+    public static void main(String[] args) {
+          //Write a program
+        System.out.println("Hello, Java course!");
+    }
+}
